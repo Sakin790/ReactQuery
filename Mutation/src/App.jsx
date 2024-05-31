a0b7a93840fbe01b630b21c5f158d4dabd058a34
Sakin790 <sakinislam79@gmail.com>
2024-05-31 22:18:22 +0600

    working

--- Mutation/src/App.jsx
+++ Mutation/src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [text, setText] = useState("");
+  console.log(text);
+
+  const getTodos = async () => {
+    const response = await axios.get("https://dummyjson.com/todos");
+    return response.data.todos;
+  };
+
+  const {
+    isLoading,
+    isError,
+    data: Todo,
+  } = useQuery({
+    queryKey: ["Todo"],
+    queryFn: getTodos,
+    staleTime: 10000,
+  });
+  console.log(Todo);
+
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+  if (isError) {
+    return <div>{isError.message}</div>;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h2>Reaciving {Todo.length} todos</h2>
+      <input
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+        type="text"
+        placeholder="Enter Todo..."
+      />
+    </div>
+  );
+};
 
-export default App
+export default App;
