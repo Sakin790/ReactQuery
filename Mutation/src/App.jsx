e9733e3c34fffa6400e31d603c27452d29be8f0d
Sakin790 <sakinislam79@gmail.com>
2024-06-05 20:32:05 +0600

    UUIDV4

--- Mutation/src/App.jsx
+++ Mutation/src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import { fetchPosts } from "./api/api";
 import { createPost } from "./api/api";
+import { deletePost } from "./api/api";
 import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
+import { v4 as uuidv4 } from "uuid";
 
 const App = () => {
   const queryClient = useQueryClient();
@@ -17,9 +19,10 @@ const App = () => {
 
   const handleAddPost = (post) => {
     createPostMutation.mutate({
-      id: Date.now(),
+      id: uuidv4(),
       title: post,
     });
+
     setPost("");
   };
 
@@ -40,6 +43,18 @@ const App = () => {
     queryFn: fetchPosts,
   });
 
+  const deletePostMutation = useMutation({
+    mutationFn: deletePost,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["posts"] });
+    },
+  });
+
+  const handleDelete = (id) => {
+    deletePostMutation.mutate(id);
+    console.log("Object deleted!");
+  };
+
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error: {error.message}</div>;
 
@@ -58,6 +73,13 @@ const App = () => {
       {posts.map((post) => (
         <div key={post.id}>
           <h4 style={{ cursor: "pointer" }}>{post.title}</h4>
+          <button
+            onClick={() => {
+              handleDelete(post.id);
+            }}
+          >
+            Delete
+          </button>
         </div>
       ))}
     </div>
